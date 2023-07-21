c737b4710bdd25a7a9fc82dd74d00e99f2749e15
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2023-07-21 15:53:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ echo "First, we install the required apt packages"
 sudo add-apt-repository ppa:inivation-ppa/inivation
 sudo apt-get update
 sudo apt-get install            \
+    python3-vcstools            \
     ros-$1-sophus               \
     ros-$1-pcl-ros              \
     ros-$1-eigen-conversions    \
     ros-$1-camera-info-manager  \
     ros-$1-image-view           \
-    libcaer-dev                 \
+   #libcaer-dev                 \
     libfftw3-dev libfftw3-doc   \
     libglew-dev                 \
    #libopencv-dev               \
